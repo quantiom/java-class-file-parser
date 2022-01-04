752d0f08cce54e5d02fb3da7bfc3326ee43b5322
quantiom <quantioms@gmail.com>
2022-01-04 10:40:04 -0500

    Add WIP constant pool parsing

--- java-class-file-parser/parser/JavaClass.cpp
+++ java-class-file-parser/parser/JavaClass.cpp
@@ -1,48 +1,84 @@
+#include <optional>
 #include "JavaClass.h"
 
 void JavaClass::parse() {
-	if (this->read_u1(0) != 0xCA || this->read_u1(1) != 0xFE || this->read_u1(2) != 0xBA || this->read_u1(3) != 0xBE) {
+	if (this->read_u1() != 0xCA || this->read_u1() != 0xFE || this->read_u1() != 0xBA || this->read_u1() != 0xBE) {
 		throw std::invalid_argument("Invalid java class file. Does not have 0xCAFEBABE magic.");
 	}
 
-	this->m_minor_version = this->read_u2(4);
-	this->m_major_version = this->read_u2(6);
+	this->m_minor_version = this->read_u2();
+	this->m_major_version = this->read_u2();
 
 	this->parse_constant_pool();
 }
 
 void JavaClass::parse_constant_pool() {
-	uint32_t constant_pool_count = this->read_u2(8);
-
-	const uint32_t start = 10;
-	uint32_t offset = 0;
+	const auto constant_pool_count = this->read_u2();
 
 	for (int i = 1; i < constant_pool_count; i++) {
-		const auto tag = (ConstantPoolType)this->read_u1(start + offset);
+		const auto tag = this->read_u1();
+		std::optional<std::vector<u1>> byte_info;
 
-		switch (tag) {
-			case ConstantPoolType::CONSTANT_Methodref:
-				auto class_index = this->read_u2(start + offset + 1);
-				auto name_and_type_index = this->read_u2(start + offset + 3);
+		switch ((ConstantPoolType)tag) {
+			case ConstantPoolType::CONSTANT_Fieldref:
+			case ConstantPoolType::CONSTANT_InterfaceMethodref:
+			case ConstantPoolType::CONSTANT_Methodref: {
+				const auto class_index = this->read_u2();
+				const auto name_and_type_index = this->read_u2();
+				
+				byte_info = { (u1)(class_index >> 8), (u1)class_index, (u1)(name_and_type_index >> 8), (u1)name_and_type_index };
 
-				std::cout << "class index: " << (int)class_index << '\n';
-				std::cout << "name and type index: " << (int)name_and_type_index << '\n';
+				//std::cout << "class index: " << (int)class_index << '\n';
+				//std::cout << "name and type index: " << (int)name_and_type_index << '\n';
+			} break;
+			case ConstantPoolType::CONSTANT_Class:
+			case ConstantPoolType::CONSTANT_String: {
+				const auto name_index = this->read_u2();
 
-				break;
+				byte_info = { (u1)(name_index >> 8), (u1)name_index };
+			} break;
+			case ConstantPoolType::CONSTANT_Integer:
+			case ConstantPoolType::CONSTANT_Float: {
+				const auto value = this->read_u2();
+				const auto bytes = (u1*)&value;
+
+				byte_info = { bytes[0], bytes[1], bytes[2], bytes[3] };
+			} break;
+
 		}
 
-		break;
+		if (byte_info.has_value()) {
+			this->m_constant_pool.push_back(ConstantPoolEntryInfo{ tag, byte_info.value() });
+		}
 	}
 }
 
-uint32_t JavaClass::read_u4(uint32_t start_idx) {
+u4 JavaClass::read_u4() {
+	const auto ret = this->read_u4(this->m_current_byte_index);
+	this->m_current_byte_index += 4;
+	return ret;
+}
+
+u2 JavaClass::read_u2() {
+	const auto ret = this->read_u2(this->m_current_byte_index);
+	this->m_current_byte_index += 2;
+	return ret;
+}
+
+u1 JavaClass::read_u1() {
+	const auto ret = this->read_u1(this->m_current_byte_index);
+	this->m_current_byte_index += 1;
+	return ret;
+}
+
+u4 JavaClass::read_u4(size_t start_idx) {
 	return this->m_bytes[start_idx] + this->m_bytes[start_idx + 1] + this->m_bytes[start_idx + 2] + this->m_bytes[start_idx + 3];
 }
 
-uint16_t JavaClass::read_u2(uint32_t start_idx) {
+u2 JavaClass::read_u2(size_t start_idx) {
 	return this->m_bytes[start_idx] + this->m_bytes[start_idx + 1];
 }
 
-uint32_t JavaClass::read_u1(uint32_t idx) {
+u1 JavaClass::read_u1(size_t idx) {
 	return this->m_bytes[idx];
 }
--- java-class-file-parser/parser/JavaClass.h
+++ java-class-file-parser/parser/JavaClass.h
@@ -2,23 +2,56 @@
 #include <iostream>
 #include <vector>
 
-#include "ConstantPool.h"
+using u4 = uint32_t;
+using u2 = uint16_t;
+using u1 = uint8_t;
+
+enum class ConstantPoolType {
+	CONSTANT_Utf8 = 1,
+	CONSTANT_Integer = 3,
+	CONSTANT_Float = 4,
+	CONSTANT_Long = 5,
+	CONSTANT_Double = 6,
+	CONSTANT_Class = 7,
+	CONSTANT_String = 8,
+	CONSTANT_Fieldref = 9,
+	CONSTANT_Methodref = 10,
+	CONSTANT_InterfaceMethodref = 11,
+	CONSTANT_NameAndType = 12,
+	CONSTANT_MethodHandle = 15,
+	CONSTANT_MethodType = 16,
+	CONSTANT_InvokeDynamic = 18
+};
+
+struct ConstantPoolEntryInfo {
+	u1 tag;
+	std::vector<u1> info;
+};
 
 class JavaClass {
 public:
-	JavaClass(std::vector<uint8_t> bytes) : m_bytes(bytes) {};
+	JavaClass(std::vector<uint8_t> bytes) : m_bytes(bytes) {
+		this->m_current_byte_index = 0;
+	};
 
 	void parse();
 
 private:
 	void parse_constant_pool();
 
-	uint32_t read_u4(uint32_t start_idx);
-	uint16_t read_u2(uint32_t start_idx);
-	uint32_t read_u1(uint32_t idx);
+	u4 read_u4();
+	u2 read_u2();
+	u1 read_u1();
 
-	uint16_t m_minor_version;
-	uint16_t m_major_version;
+	u4 read_u4(size_t start_idx);
+	u2 read_u2(size_t start_idx);
+	u1 read_u1(size_t idx);
 
-	std::vector<uint8_t> m_bytes;
+	u2 m_minor_version;
+	u2 m_major_version;
+
+	std::vector<ConstantPoolEntryInfo> m_constant_pool;
+
+	size_t m_current_byte_index;
+	std::vector<u1> m_bytes;
 };
